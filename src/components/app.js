722621f9235c78dc1cc9bf16de0ad6a7119c43af
Ravi Kant Singh <raysk7161@gmail.com>
2020-06-06 03:35:51 +0530

    ready for start building

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>React App</p>
+      </div>
+    );
+  }
+}
+
+export default App;
